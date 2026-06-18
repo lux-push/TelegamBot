0c7eddc348d869f4f8c879e132c0e8dad8a5cfaa
Никита Ивлев <ivlev@example.com>
2026-06-18 14:30:17 +0700

    TelegramBot_full

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 # 🔥 ВСТАВЬ СВОИ ДАННЫЕ ЗДЕСЬ:
 BOT_A_TOKEN = "8524109700:AAELiD2Q-ZjvpGGGh0QSjiQ13nOOYgW_Ccg"  # Твой токен @avito_kufar
 BOT_B_TOKEN = "8219579141:AAHO-WbZauDFdGh09Q14ghgmMjeKzgsjtBk" # Твой токен @avito_manager_bot
-ADMIN_IDS = {1737278909, 8441226676}
+ADMIN_IDS = {1737278909, 722513807}
 ADMIN_CHAT_ID = 1737278909
 API_PORT = 8080
